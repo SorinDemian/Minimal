643c05303212f1a2f0890194639a842768551e65
SorinDemian <63290714+SorinDemian@users.noreply.github.com>
2021-05-11 18:33:03 +0300

    Update and rename index.html to index.ml

--- index.ml
+++ index.ml
@@ -1,17 +1,5 @@
 ## Welcome to GitHub Pages
 
-<script> 
-  document.cookie = "session=test GDPR"; 
-  document.cookie = "favorite_task=collect Data"; 
-  function alertCookie() { 
-  alert(document.cookie); 
-  } 
-</script>
-
-<body> 
-  Bine ai venit la lab 
-  <button onclick="alertCookie()">Show cookies</button> 
-</body>
 
 You can use the [editor on GitHub](https://github.com/SorinDemian/Minimal/edit/gh-pages/index.md) to maintain and preview the content for your website in Markdown files.
 
